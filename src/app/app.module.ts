ec9c063e2ed1eed2003af95a2a90d2776959e42c
Ritik <ritikraj2699@outlook.com>
2024-02-06 20:18:58 +0530

    generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
+import { CardComponent } from './components/card/card.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { SkeletonComponent } from './components/skeleton/skeleton.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    SearchBarComponent,
+    PaginationComponent,
+    SkeletonComponent
   ],
   imports: [
     BrowserModule,
