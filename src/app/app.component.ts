80bff5976e0688d20bda26bc25889ee67f775895
Ritik <ritikraj2699@outlook.com>
2024-02-06 20:43:46 +0530

    modified app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,12 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   constructor(
     private apiService: ApiService
   ) {}
-  
-  title:string = 'fyle-frontend-challenge';
+
+  title: string = 'fyle-frontend-challenge';
   username: string = '';
   user: any;
   repos: any[] = [];
@@ -20,10 +20,54 @@ export class AppComponent implements OnInit{
   currentPage: number = 1;
   itemsPerPage: number = 10;
   totalRepos: number = 1;
-  
+
+  ngOnInit() {
+    if (this.username === '') {
+      return;
+    }
+
+    this.loadUser();
+    this.getTotalReposCount();
+    this.loadRepos();
+  }
+
   searchRepositories(userInput: string) {
     this.username = userInput;
     this.ngOnInit();
+  }
+
+  loadUser() {
+    this.apiService.getUser(this.username).subscribe(
+      (user) => {
+        this.user = user;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getTotalReposCount() {
+    this.apiService.getTotalReposCount(this.username).subscribe(
+      (repos) => {  
+        this.totalRepos = (repos as any[]).length;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  loadRepos() {
+    this.apiService.getRepos(this.username, this.currentPage, this.itemsPerPage).subscribe(
+      (repos) => {
+        this.repos = (repos as any[]).map((repo: any) => ({...repo, languages: []}));
+        this.loadLanguages();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   loadLanguages() {
@@ -49,51 +93,11 @@ export class AppComponent implements OnInit{
     this.loadRepos();
   }
 
-
-   getTotalReposCount() {
-    this.apiService.getTotalReposCount(this.username).subscribe(
-      (repos) => {  
-        this.totalRepos = (repos as any[]).length;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
-
-  loadRepos() {
-    this.apiService.getRepos(this.username,this.currentPage,this.itemsPerPage).subscribe(
-      (repos) => {
-        this.repos = (repos as any[]).map((repo: any) => ({...repo, languages: []}));
-        this.loadLanguages();
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
-
   get totalPages(): number {
     return Math.ceil(this.totalRepos / this.itemsPerPage);
   }
 
   get pages(): number[] {
     return Array.from({ length: this.totalPages }, (_, i) => i + 1);
-  }
-  
-  ngOnInit() {
-
-
-    this.apiService.getUser(this.username).subscribe(user => {
-      this.user = user;
-      this.loading = false;
-    });
-    
-    if (this.username === '') {
-      return;
-    }
-
-    this.getTotalReposCount();
-    this.loadRepos();
   }
 }
