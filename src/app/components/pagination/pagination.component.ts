348b5310a414485ed8dc40ab49f36bdfdb9d2ded
Ritik <ritikraj2699@outlook.com>
2024-02-06 20:24:13 +0530

    modified pagination component

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent {
+  @Input() currentPage: number = 1;
+  @Input() itemsPerPage: number = 10;
+  @Input() totalPages: number = 1;
+  @Input() pages: number[] = [];
+  @Output() onPageChange: EventEmitter<number> = new EventEmitter<number>();
+  @Output() changeItemsPerPage: EventEmitter<number> = new EventEmitter<number>();
 
+  triggerPageChange(newPage: number): void {
+    this.onPageChange.emit(newPage);
+  }
+
+  triggerItemsPerPageChange(event: Event): void {
+    const newItemsPerPage = (event.target as HTMLSelectElement).value;
+    this.changeItemsPerPage.emit(Number(newItemsPerPage));
+  }
 }
