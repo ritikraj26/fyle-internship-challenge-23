d9425edbada3a51b63fe11bfa66b6d4eb6b97549
Ritik <ritikraj2699@outlook.com>
2024-02-06 20:27:57 +0530

    created services and added github token

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap, throwError } from 'rxjs';
 
@@ -7,13 +7,32 @@ import { tap, throwError } from 'rxjs';
 })
 export class ApiService {
 
+  accessToken: string = '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';
+  headers = new HttpHeaders({
+    'Authorization': `token ${this.accessToken}`
+  });
+
   constructor(
     private httpClient: HttpClient
   ) { }
 
   getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`, {headers: this.headers});
   }
 
   // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
+
+  getTotalReposCount(githubUsername: string) {
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos`, {headers: this.headers});
+  }
+
+  getRepos(githubUsername: string, page: number, pageSize: number) {
+    const params = {page: page.toString(), per_page: pageSize.toString()};
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos`,{params: params, headers: this.headers});
+  }
+
+  getLanguages(githubUsername: string, repoName: string) {
+    return this.httpClient.get(`https://api.github.com/repos/${githubUsername}/${repoName}/languages`, {headers: this.headers});
+  }
+
 }
